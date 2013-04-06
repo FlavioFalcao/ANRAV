daafde9a03ca0258fb925cd908c3c62c3e190fb7
Kyle Owen <kyleowen@kyle-macbook.local>
2013-04-05 22:19:55 -0500

    Readding stuff for shell

--- ANRAV.ino
+++ ANRAV.ino
@@ -14,17 +14,42 @@ include "ANRAV.h"
 // Interrupt Service Routine which triggers when Xbee receives data.
 void ManualOverride()
 {
-	// Kyle's baby now!
-	/* 
-	Functions needed:
-	h - print all available commands
-	s [name] [parameter] - sets a parameter
-	g [name] - displays a parameter
-	n - navigation mode allows for steering the vessel via the cmd line.
-	e - exit
-	...
+
+  /* 
+  Functions needed:
+  h - print all available commands --implement last
+  s [name] [parameter] - sets a parameter
+  	consKp
+  	consKi
+  	consKd
+  	aggKp
+  	aggKi
+  	aggKd
+  	rudderAngle
+  	waypointN
+  	waypointW
+  g [name] - displays a parameter
+  n - navigation mode allows for steering the vessel via the cmd line.
+  q - terminates manual override mode
   */
+
+  //if you're here, the Arduino has been interrupted into manual control
+  //switch to manual control...does a PID controller need to know this?
   return 0;
+
+  switch(manualCommand[0])
+  {
+  	case 'q': //exit manual override
+
+  		break;
+  	case 'h': //print help via wireless
+  		//how do print via Xbee?
+  		break;
+  	case 's':
+  		break;
+
+  }
+
 }
 
 // Initialize Vessel Subsystems, etc.
